5b3b2ecd1943c356d5aa7ae87fb016042f1ea625
s b <svb.phx@gmail.com>
2017-11-10 10:38:46 -0700

    Updated new output message

--- src/main/java/com/rs/hello/Application.java
+++ src/main/java/com/rs/hello/Application.java
@@ -21,7 +21,7 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot2");
+            System.out.println("Let's inspect the beans provided by Spring Boot 3");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
